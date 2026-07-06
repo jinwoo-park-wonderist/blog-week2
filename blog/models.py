606547fddf3e4455709a4a88972ed0f5643f8dca
jinwoopark <jinwoo.park@wonderistagency.com>
2026-07-06 14:06:43 -0700

    feat(models): add Tag, Post, Comment model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import User
 from django.db import models
+
+
+class Tag(models.Model):
+    tag_content = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return self.tag_content
+
+class Post(models.Model):
+    post_author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    post_title = models.CharField(max_length=200)
+    post_content = models.TextField()
+    post_tag = models.ManyToManyField(Tag, related_name='posts', blank=True)
+
+    def __str__(self):
+        return self.post_title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    comment_author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    comment_content = models.TextField()
+
+    def __str__(self):
+        return self.comment_content
 
 # Create your models here.
