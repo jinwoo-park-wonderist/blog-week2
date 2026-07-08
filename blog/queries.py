dd931d0982d756514aaec4a064e0899bc70d27bf
jinwoopark <jinwoo.park@wonderistagency.com>
2026-07-08 15:01:32 -0700

    fix(models): add parameter and reverse order

--- blog/queries.py
+++ blog/queries.py
@@ -6,11 +6,11 @@ def get_post(post_id=None):
     qs = Post.objects.select_related("post_author").prefetch_related("post_tag")
 
     if post_id is None:
-        return qs.order_by("post_author")
+        return qs.order_by("-id")
     return get_object_or_404(qs, pk=post_id)
 
-def get_posts_by_tag(post_id):
-    return Post.objects.select_related("post_author").prefetch_related("post_tag").order_by("-id")
+def get_posts_by_tag(tag_id):
+    return Post.objects.select_related("post_author").prefetch_related("post_tag").filter(post_tag=tag_id).order_by("-id")
 
 def get_comments_by_post(post_id):
     return Comment.objects.filter(post=post_id).select_related("comment_author").order_by("id")
